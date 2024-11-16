5e9a8ff879c969fc13d6fb529ebd2e00a7b57c24
Sarin Bhattarai <sarin.bhattarai.a19@icp.edu.np>
2024-11-16 10:13:34 +0545

    feat(pseudo-element)

--- src/pages/hero/Hero.jsx
+++ src/pages/hero/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
           </motion.h1>
           <motion.div variants={textVariants} className="buttons">
             <motion.button variants={textVariants}>
-              See the Latest Works
+              See What I’ve Built
             </motion.button>
             <motion.button variants={textVariants}>Contact Me</motion.button>
           </motion.div>
